8d186d7ae9df379d18fde645f46bb6a70624a699
t0719 <t.0719.hiro.og@gmail.com>
2024-11-08 12:37:57 +0000

    backend疎通

---
+++ app/hello_backend/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+export default function Page() {
+  const [data, setData] = useState({ message: '' });
+  useEffect(() => {
+    const apipath = 'api/hello/backend';
+    axios
+      .get(apipath)
+      .then((res) => res.data)
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+  return <div>hello, {data.message}</div>;
+}
